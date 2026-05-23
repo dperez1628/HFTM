ae25c0aa290e34801ff7c24ad5f82dbc04c2ab16
David Pérez Martínez <dperez16ugr@gmail.com>
2026-05-23 08:44:39 +0200

    feat: initial Spring Boot project scaffold

---
+++ src/test/java/com/hftm/TestHftmApplication.java
@@ -0,0 +1,11 @@
+package com.hftm;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestHftmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(HftmApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
